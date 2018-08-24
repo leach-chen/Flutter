9e28d8aa96c260a6c3a2f4392a96a5bd1ddba220
leach.chen <leach>
2018-08-24 18:48:47 +0800

    modify

---
+++ Prj/flutter_memoryalbum/lib/MyDrawer.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatefulWidget
+{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new MyDrawerState();
+  }
+}
+
+class MyDrawerState extends State<MyDrawer>
+{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Container(
+      width: 300.0,
+      color: Colors.red,
+    );
+  }
+
+}
